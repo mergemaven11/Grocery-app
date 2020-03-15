2de52ccc68abce18563b17a7cd82a68de9aea52c
tscott2023 <38087336+Tobias2023@users.noreply.github.com>
2020-03-15 15:55:18 -0400

    Add FormsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,4 @@
-import { RecipeDetailComponent } from './recipe-book/recipe-detail/recipe-detail.component';
-import { RecipeItemComponent } from './recipe-book/recipe-list/recipe-item/recipe-item.component';
-import { RecipeListComponent } from './recipe-book/recipe-list/recipe-list.component';
-import { ShoppingListEditComponent } from './shopping-list/shoppingList-edit/shoppingList-edit.component';
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -10,6 +7,13 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { RecipeBookComponent } from './recipe-book/recipe-book.component';
+import { RecipeDetailComponent } from './recipe-book/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './recipe-book/recipe-list/recipe-item/recipe-item.component';
+import { RecipeListComponent } from './recipe-book/recipe-list/recipe-list.component';
+import { ShoppingListEditComponent } from './shopping-list/shoppingList-edit/shoppingList-edit.component';
+
+import { FormsModule } from '@angular/forms'
+
 
 
 
@@ -26,7 +30,8 @@ import { RecipeBookComponent } from './recipe-book/recipe-book.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
